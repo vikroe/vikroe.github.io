cff29cb5d4bb4a80d99d0028ee24616ec37c3ce6
vikroe <v.koropecky@protonmail.com>
2021-02-15 22:54:36 +0100

    Uploaded first script

---
+++ malda.js
@@ -0,0 +1,6 @@
+var path_to_file = "https://vikroe.github.io/MaldaSounds/";
+var files = ["hehe.mp3","kchr.mp3","mrrrre.mp3","woof.mp3"];
+
+for (var i = 0; i < files.length; i++){
+    console.log(path_to_file + files[i]);
+}
