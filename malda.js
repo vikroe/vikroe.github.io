618516392c70165682d1c35d8014ac6601bad22b
vikroe <v.koropecky@protonmail.com>
2021-02-16 21:28:19 +0100

    Working soundboard script

--- malda.js
+++ malda.js
@@ -1,17 +1,25 @@
+function playSound(name){
+    document.getElementById(name).play();
+}
+
 var path_to_file = "https://vikroe.github.io/MaldaSounds/";
 var files = ["hehe.mp3","kchr.mp3","mrrrre.mp3","woof.mp3"];
-var audioList = new Array(4);
+
+
 
 for (var i = 0; i < files.length; i++){
-    
-    audioList[i] = new Audio(path_to_file+files[i]);
+    var audio = document.createElement("audio");
+    audio.setAttribute("id", "audio"+String(i));
+    audio.setAttribute("src", path_to_file+files[i]);
+
+    console.log(audio);
 
     var button = document.createElement("button");
+    button.setAttribute("onclick", "document.getElementById(\"audio" + i + "\").play();");
     button.innerHTML = "Do Something " + String(i);
 
     var body = document.getElementById("buttons");
+    body.appendChild(audio);
     body.appendChild(button);
-
-    button.addEventListener("click", function() {audioList[i].play()});
-
 }
+
