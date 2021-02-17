0f43e0388bb9bc5be3825fabc45b72420c1b05be
vikroe <v.koropecky@protonmail.com>
2021-02-17 23:40:16 +0100

    Working soundboard

--- malda.js
+++ malda.js
@@ -3,20 +3,20 @@ function playSound(name){
 }
 
 var path_to_file = "https://vikroe.github.io/MaldaSounds/";
-var files = ["hehe.mp3","kchr.mp3","mrrrre.mp3","woof.mp3"];
+var files = ["hehe","kchr","mrrrre","woof"];
 
 
 
 for (var i = 0; i < files.length; i++){
     var audio = document.createElement("audio");
     audio.setAttribute("id", "audio"+String(i));
-    audio.setAttribute("src", path_to_file+files[i]);
+    audio.setAttribute("src", path_to_file+files[i]+".mp3");
 
     console.log(audio);
 
     var button = document.createElement("button");
     button.setAttribute("onclick", "document.getElementById(\"audio" + i + "\").play();");
-    button.innerHTML = "Do Something " + String(i);
+    button.innerHTML = files[i];
 
     var body = document.getElementById("buttons");
     body.appendChild(audio);
