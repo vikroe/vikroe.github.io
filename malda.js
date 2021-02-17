3b296cd1eac730a11769c4644edf3f86818b10d6
vikroe <v.koropecky@protonmail.com>
2021-02-17 23:44:50 +0100

    Update malda.js

--- malda.js
+++ malda.js
@@ -1,7 +1,3 @@
-function playSound(name){
-    document.getElementById(name).play();
-}
-
 var path_to_file = "https://vikroe.github.io/MaldaSounds/";
 var files = ["hehe","kchr","mrrrre","woof"];
 
@@ -11,8 +7,6 @@ for (var i = 0; i < files.length; i++){
     var audio = document.createElement("audio");
     audio.setAttribute("id", "audio"+String(i));
     audio.setAttribute("src", path_to_file+files[i]+".mp3");
-
-    console.log(audio);
 
     var button = document.createElement("button");
     button.setAttribute("onclick", "document.getElementById(\"audio" + i + "\").play();");
